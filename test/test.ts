409fb839961ec8680db80cda6e4cdcc77742f27e
ThanksSkeleton <Thanks.Skeleton@gmail.com>
2022-04-30 03:27:57 -0700

    fix async chai tests and add additional test

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,6 @@
 import { expect } from "chai";
+import chai = require("chai");
+
 import { ethers } from "hardhat";
 import * as fs from "fs";
 import { ContractFactory, Contract } from "ethers";
@@ -6,49 +8,11 @@ import { Verifier as HCVerifier } from "../typechain-types/HandCommitment_verifi
 import { Verifier as NWVerifier } from "../typechain-types/NoWitch_verifier.sol";
 import { Verifier as VMVerifier } from "../typechain-types/ValidMove_verifier.sol";
 import { ZkWitches } from "../typechain-types/zkWitches.sol";
+import chaiAsPromised = require("chai-as-promised");
 
-/*
-describe("Verifier Contract", function () {
-    let Verifier;
-    let verifier;
+chai.use(chaiAsPromised);
 
-    beforeEach(async function () {
-        Verifier = await ethers.getContractFactory("Verifier");
-        verifier = await Verifier.deploy();
-        await verifier.deployed();
-    });
-
-    it("Should return true for correct proofs", async function () {
-        for (var i = 0; i < 16; i++) {
-            var array = JSON.parse("[" + fs.readFileSync("./circuits/build/zkWitches/" + i.toString() + "/call.json") + "]");
-            expect(await verifier.verifyProof(array[0], array[1], array[2], array[3])).to.be.true;
-        }
-    });
-    it("Should return false for invalid proof", async function () {
-        let a = [0, 0];
-        let b = [[0, 0], [0, 0]];
-        let c = [0, 0];
-        let d = Array(65).fill(0);
-        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
-    });
-});
-*/
-describe("zkWitches Contract2", function () {
-    // let a = [];
-    // let b = [];
-    // let c = [];
-    // let d = [];
-    // let signers;
-    // let balance;
-    // let tx;
-
-    // for (var i = 0; i < 16; i++) {
-    //     var array = JSON.parse("[" + fs.readFileSync("./circuits/build/zkWitches/" + i.toString() + "/call.json") + "]");
-    //     a.push(array[0]);
-    //     b.push(array[1]);
-    //     c.push(array[2]);
-    //     d.push(array[3]);
-    // };
+describe("zkWitches Contract", function () {
 
     let hc_Verifier: HCVerifier;
     let vm_Verifier: VMVerifier;
@@ -92,19 +56,19 @@ describe("zkWitches Contract2", function () {
 	it("HCVerifier Verifies Correctly", async function() 
     {
         var hccall_array = JSON.parse("[" + fs.readFileSync(hccall) + "]");
-        expect(await hc_Verifier.verifyProof(hccall_array[0], hccall_array[1], hccall_array[2], hccall_array[3])).to.be.true;
+        await expect(hc_Verifier.verifyProof(hccall_array[0], hccall_array[1], hccall_array[2], hccall_array[3])).to.be.eventually.true;
     }); 
 	
 	it("VMVerifier Verifies Correctly", async function() 
     {
         var vmcall_array = JSON.parse("[" + fs.readFileSync(vmcall) + "]");
-        expect(await vm_Verifier.verifyProof(vmcall_array[0], vmcall_array[1], vmcall_array[2], vmcall_array[3])).to.be.true;
+        await expect(vm_Verifier.verifyProof(vmcall_array[0], vmcall_array[1], vmcall_array[2], vmcall_array[3])).to.be.eventually.true;
     }); 
 
 	it("NWVerifier Verifies Correctly", async function() 
     {
         var nwcall_array = JSON.parse("[" + fs.readFileSync(nwcall) + "]");
-        expect(await nw_Verifier.verifyProof(nwcall_array[0], nwcall_array[1], nwcall_array[2], nwcall_array[3])).to.be.true;
+        await expect(nw_Verifier.verifyProof(nwcall_array[0], nwcall_array[1], nwcall_array[2], nwcall_array[3])).to.be.eventually.true;
     }); 
 
 	it("HC Verifier REJECTS incorrect proof", async function() 
@@ -112,15 +76,32 @@ describe("zkWitches Contract2", function () {
         var hccall_array = JSON.parse("[" + fs.readFileSync(hccall) + "]");
         var nwcall_array = JSON.parse("[" + fs.readFileSync(nwcall) + "]");
 
-        expect(await hc_Verifier.verifyProof(hccall_array[0], hccall_array[1], nwcall_array[2], hccall_array[3])).to.be.false;
+        await expect(hc_Verifier.verifyProof(hccall_array[0], hccall_array[1], nwcall_array[2], hccall_array[3])).to.be.eventually.false;
     }); 
 	
 	// ZKWitches Contract Tests
 	
-	it("ZKWitches: can join new empty game", async function() 
+	it("Can join new empty game", async function() 
     {
+        var hccall_array = JSON.parse("[" + fs.readFileSync(hccall) + "]");
+        await expect(zkWitches.JoinGame(hccall_array[0], hccall_array[1], hccall_array[2], hccall_array[3])).to.not.be.rejected;
     }); 
 	
+	it("Cannot Join with incorrect proof", async function() 
+    {
+        var hccall_array = JSON.parse("[" + fs.readFileSync(hccall) + "]");
+        var nwcall_array = JSON.parse("[" + fs.readFileSync(nwcall) + "]");
+
+        await expect(zkWitches.JoinGame(hccall_array[0], hccall_array[1], nwcall_array[2], hccall_array[0][0])).to.be.rejected;
+    }); 
+
+	it("Cannot join the same game twice", async function() 
+    {
+        var hccall_array = JSON.parse("[" + fs.readFileSync(hccall) + "]");
+        await expect(zkWitches.JoinGame(hccall_array[0], hccall_array[1], hccall_array[2], hccall_array[3])).to.not.be.rejected;
+        await expect(zkWitches.JoinGame(hccall_array[0], hccall_array[1], hccall_array[2], hccall_array[3])).to.be.rejected;
+    }); 
+
 	// a lot more needed.
 
 });
